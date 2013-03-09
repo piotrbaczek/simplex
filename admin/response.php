0801efe90644028836424dee0a4aa572bccd026d
PETTER <PETTER@PETTER-LAPTOP>
2013-03-09 11:27:53 +0100

    text formatting

--- admin/response.php
+++ admin/response.php
@@ -1,11 +1,11 @@
 <?php
+
 include '../classes/login.class.php';
-$k=new login();
-if($k->logIn($_POST['login'], $_POST['password'])){
+if (new login($_POST['login'], $_POST['password'])) {
     session_start();
-    $_SESSION['admin']='true';
+    $_SESSION['admin'] = 'true';
     header('Location:admin.php');
-}else{
+} else {
     header('Location:index.php?error=1');
 }
 ?>
