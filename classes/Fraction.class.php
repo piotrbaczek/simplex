c001016f322a45d460f2079731d3f8c5ac04dbe6
PETTER <PETTER@PETTER-LAPTOP>
2013-03-09 10:00:16 +0100

    Big M theory in some functions - continuation

--- classes/Fraction.class.php
+++ classes/Fraction.class.php
@@ -1,345 +1,305 @@
 <?php
 
-/**
- * Trida Fraction pro praci se zlomky.
- *
- * Trida Fraction obsahuje vsechny dulezite procedury pro praci se zlomky :
- *  * Kraceni zlomku na zakladni tvar
- *  * Predavedeni realnych cisel na zlomky
- *  * Zakladni matematické operace
- *    * Scitani
- *    * Odecitani
- *    * Nasobeni
- *    * Deleni
- *
- * @author Tomas Lang <tomas.lang@gmail.com>
- * @copyright Copyright (c) 2008+, Tomas Lang
- * @version 1.2
- * @category Mathematic
- * @package Basic
- */
-class Fraction {
-	/**
-	 * @var int $numerator Citatel zlomku
-	 */
-	private $numerator;
-	/**
-	 * @var int $denominator Jmenovatel zlomlu
-	 */
-	private $denominator;
+class Fraction2 {
 
-	/**
-	 * Konstruktor
-	 *
-	 * Vytvori novou instanci tridy zlomek, nacte citatel a jmenovatel a
-	 * nakonec zlomek zkrati do zakladniho tvaru.
-	 *
-	 * @param float $numerator Citatel zlomku
-	 * @param float $denominator Jmenovatel zlomlu
-	 *
-	 * @todo: $number | $denominator = object Fraction
-	 */
-	public function __construct( $numerator = 1, $denominator = 1 ) {
-		$numerator = $this->realToFraction( $numerator );
-		$denominator = $this->realToFraction( $denominator );
-			
-		$this->numerator = (int)( $numerator[ 0 ] * $denominator[ 1 ] );
-		$this->denominator = (int)( $denominator[ 0 ] * $numerator[ 1 ] );
-			
-		if ( $this->denominator == 0 ) {
-			throw new Exception( 'Denominator can\'t be 0!' );
-		}
-			
-		$this->reduction();
-	}
+    private $numerator;
+    private $denominator;
+    private $mnumerator;
+    private $mdenominator;
 
-	/**
-	 * Funkce prevede realne cislo do tvaru zlomku
-	 *
-	 * @param float $number
-	 *
-	 * @return array Pole o dvou prvcich (citatel, jmenovatel zlomku)
-	 */
-	public function realToFraction( $number ) {
-		$endOfNumber = $number - (int)$number;
-		if ( $endOfNumber != 0 ) {
-			$mul = bcpow( 10, strlen( $endOfNumber ) - 2 );
-			return array( $number * $mul, $mul );
-		} else {
-			return array( $number, 1 );
-		}
-	}
+    public function __construct($numerator = 1, $denominator = 1, $mnumerator = 0, $mdenominator = 1) {
+        if (is_numeric($numerator) && is_numeric($denominator) && is_numeric($mnumerator) && is_numeric($mdenominator)) {
+            if ($denominator == 0 || $mdenominator == 0) {
+                throw new Exception('Denominator can\'t be a 0!');
+            } else {
+                $this->numerator = (int)$numerator;
+                $this->denominator = (int)$denominator;
+                $this->mnumerator = (int)$mnumerator;
+                $this->mdenominator = (int)$mdenominator;
+                $this->reduction();
+            }
+        }else{
+            $this->errormessage('Fractions must contain numbers!');
+        }
+    }
 
-	/**
-	 * Funkce vraci citatel zlomku
-	 *
-	 * @return float $this->numerator Citatel zlomku
-	 */
-	public function getNumerator() {
-		return $this->numerator;
-	}
+    public function getNumerator() {
+        return $this->numerator;
+    }
 
-	/**
-	 * Funkce vraci jmenovatel zlomku
-	 *
-	 * @return float $this->denominator Jmenovatel zlomku
-	 */
-	public function getDenominator() {
-		return $this->denominator;
-	}
+    public function getDenominator() {
+        return $this->denominator;
+    }
 
-	/**
-	 * Funkce zkrati zlomek do zakladniho tvaru
-	 *
-	 * Pokud je citatel i jmenovatel zaporny, pak je zlomek preveden na
-	 * kladny, pokud je zaporny pouze jmenovatel, je zapornost prenesena
-	 * na citatele a jmenovatel je preveden na kladny, nasledne pak je
-	 * zlomek zkracen do zakladniho tvaru na zaklade nalezeného nejvysiho
-	 * spolecného jmenovatele.
-	 */
-	public function reduction() {
-		if ( ( $this->numerator < 0 && $this->denominator < 0 ) || ( $this->denominator < 0 ) ) {
-			$this->expansion( -1 );
-		}
-		$hcd = $this->highestCommonDivisor( $this->numerator, $this->denominator );
-		$this->numerator /= $hcd;
-		$this->denominator /= $hcd;
-	}
+    public function getMNumerator() {
+        return $this->mnumerator;
+    }
 
-	/**
-	 * Funkce vraci hodnotu zlomku v realném cisle
-	 *
-	 * @return float
-	 */
-	public function getRealValue() {
-		return $this->numerator / $this->denominator;
-	}
+    public function getMDenominator() {
+        return $this->mdenominator;
+    }
 
-	/**
-	 * Funkce vraci nejvysiho spolecného delitele cilel $a a $b
-	 *
-	 * @param int $a
-	 * @param int $b
-	 */
-	public function highestCommonDivisor( $a, $b ) {
-		$a = abs( $a );
-		while ( $a != $b ) {
-			if ( $a > $b ) {
-				$a = $a - $b;
-			}else {
-				$b = $b - $a;
-			}
-		}
-		return $a;
-	}
+    private function reduction() {
+        if ($this->numerator == 0) {
+            $this->denominator = 1;
+        } elseif (( $this->numerator < 0 && $this->denominator < 0 ) || ( $this->denominator < 0 )) {
+            $this->expansion(-1);
+            $hcd = $this->highestCommonDivisor($this->numerator, $this->denominator);
+            $this->numerator /= $hcd;
+            $this->denominator /= $hcd;
+        } else {
+            $hcd = $this->highestCommonDivisor($this->numerator, $this->denominator);
+            $this->numerator /= $hcd;
+            $this->denominator /= $hcd;
+        }
 
-	/**
-	 * Funkce vraci nejmensi spolecny nasobek cisel $a a $b
-	 *
-	 * @param float $a
-	 * @param float $b
-	 */
-	private function leastCommonMultiple( $a, $b ) {
-		return ( $a * $b ) / $this->highestCommonDivisor( $a, $b );
-	}
+        if ($this->mnumerator == 0) {
+            $this->mdenominator = 1;
+        } elseif (( $this->mnumerator < 0 && $this->mdenominator < 0 ) || ( $this->mdenominator < 0 )) {
+            $this->mexpansion(-1);
+            $hcd = $this->highestCommonDivisor($this->mnumerator, $this->mdenominator);
+            $this->mnumerator /= $hcd;
+            $this->mdenominator /= $hcd;
+        } else {
+            $hcd = $this->highestCommonDivisor($this->mnumerator, $this->mdenominator);
+            $this->mnumerator /= $hcd;
+            $this->mdenominator /= $hcd;
+        }
+    }
 
-	/**
-	 * Funkce prevede tento a zadany zlomek, na zlomly se spolecnym
-	 * jmenovatelem
-	 *
-	 * @param object Fraction
-	 */
-	public function commonDenominator( &$fraction ) {
-		$lcm = $this->leastCommonMultiple( $this->denominator, $fraction->denominator );
-		$this->numerator = $this->numerator * ( $lcm / $this->denominator );
-		$fraction->numerator = $fraction->numerator * ( $lcm / $fraction->denominator );
-		$this->denominator = $fraction->denominator = $lcm;
-	}
+    public function expansion($num) {
+        $this->numerator *= $num;
+        $this->denominator *= $num;
+    }
 
-	/**
-	 * Funkce rozsiri zlomek o hodnotu zlomku ci cisla $num
-	 *
-	 * @param object|float Fraction
-	 */
-	private function expansion( $num ) {
-		$this->numerator *= $num;
-		$this->denominator *= $num;
-	}
+    public function mexpansion($num) {
+        $this->mnumerator *= $num;
+        $this->mdenominator *= $num;
+    }
 
-	/**
-	 * Funkce zkrati zlomek o hodnotu zlomku ci cisla $parametr
-	 *
-	 * @param object|float
-	 */
-	private function contraction( $parametr ) {
-		$this->numerator /= $num;
-		$this->denominator /= $num;
-	}
+    public function contraction($num) {
+        $this->numerator /= $num;
+        $this->denominator /= $num;
+    }
 
-	/**
-	 * Pricte zadany zlomek ci cislo k tomuto zlomku a zkrati jej na
-	 * zakladni tvar
-	 *
-	 * @param object|float Fraction
-	 */
-	public function add( $parametr ) {
-		if ( !( $parametr instanceOf Fraction ) && is_numeric( $parametr ) ) {
-			$parametr = new Fraction( $parametr );
-			$this->commonDenominator( $parametr );
-			$this->numerator = $this->numerator + $parametr->numerator;
-		} elseif($parametr instanceof Fraction) {
-			$newdenominator=$this->denominator*$parametr->getDenominator();
-			$newnumerator=($this->numerator*$parametr->getDenominator())+($parametr->getNumerator()*$this->denominator);
-			$this->numerator=$newnumerator;
-			$this->denominator=$newdenominator;
-		}else{
-			throw new Exception( 'Parametr must be fraction or number!' );
-		}
-		$this->reduction();
-	}
+    public function compare($param) {
+        if ($param instanceof Fraction2) {
+            $a = $this->numerator * $param->getDenominator();
+            $b = $this->denominator * $param->getNumerator();
+            if ($a > $b) {
+                return true;
+                //this fraction is bigger
+            } else {
+                return false;
+                //param fraction is bigger
+            }
+        } elseif (is_numeric($param)) {
+            $p = $this->realToFraction($param);
+            $a = $this->numerator * $p[1];
+            $b = $this->denominator * $p[0];
+            if ($a > $b) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
-	/**
-	 * Odecte zadany zlomek ci cislo od tohoto zlomku a zkrati jej na
-	 * zakladni tvar
-	 *
-	 * @param object|float Fraction
-	 */
-	public function sub( $parametr ) {
-		if ( !( $parametr instanceOf Fraction ) && is_numeric( $parametr ) ) {
-			$parametr = new Fraction( $parametr );
-			$this->commonDenominator( $parametr );
-			$this->numerator = $this->numerator - $parametr->numerator;
-		} elseif($parametr instanceof Fraction){
-			$newdenominator=$this->denominator*$parametr->getDenominator();
-			$newnumerator=($this->numerator*$parametr->getDenominator())-($parametr->getNumerator()*$this->denominator);
-			$this->numerator=$newnumerator;
-			$this->denominator=$newdenominator;
-		}else{
-			throw new Exception( 'Parametr must be fraction or number!' );
-		}
-		$this->reduction();
-	}
+    public static function isPositive($param) {
+        if ($param instanceof Fraction2) {
+            return $param->numerator > 0 ? true : false;
+        } elseif (is_numeric($param)) {
+            return $param > 0 ? true : false;
+        }
+    }
 
-	/**
-	 * Vynasobi tento zlomek zadanym zlomkem ci cislem a zkrati jej na
-	 * zakladni tvar
-	 *
-	 * @param object|float Fraction
-	 */
-	public function multiplication( $parametr ) {
-		if ( $parametr instanceOf Fraction ) {
-			$numerator = $parametr->numerator;
-			$denominator = $parametr->denominator;
-		} elseif ( is_numeric( $parametr ) ) {
-			$parametr = $this->realToFraction( $parametr );
-			$numerator = $parametr[ 0 ];
-			$denominator = $parametr[ 1 ];
-		} else {
-			throw new Exception( 'Parametr must be fraction or number!' );
-		}
-			
-		$this->numerator *= (int)$numerator;
-		$this->denominator *= (int)$denominator;
-			
-		$this->reduction();
-	}
+    public static function isNegative($param) {
+        if ($param instanceof Fraction2) {
+            return $param->numerator < 0 ? true : false;
+        } elseif (is_numeric($param)) {
+            return $param < 0 ? true : false;
+        }
+    }
 
-	/**
-	 * Vydeli tento zlomek zadanym zlomkem ci cislem a zkrati jej na
-	 * zakladni tvar
-	 *
-	 * @param object|float Fraction
-	 */
-	public function division( $parametr ) {
-		if ( $parametr instanceOf Fraction ) {
-			$numerator = $parametr->numerator;
-			$denominator = $parametr->denominator;
-		} elseif ( is_numeric( $parametr ) ) {
-			$parametr = $this->realToFraction( $parametr );
-			$numerator = $parametr[ 0 ];
-			$denominator = $parametr[ 1 ];
-		} else {
-			throw new Exception( 'Parametr must be fraction or number!' );
-		}
+    public function reverse() {
+        $sign = 1;
+        $numerator = $this->numerator;
+        $denominator = $this->denominator;
+        if ($this->numerator < 0) {
+            $sign = -1;
+            $this->numerator*=$sign;
+        }
+        $this->numerator = $denominator;
+        $this->denominator = $numerator;
+        if ($sign == -1) {
+            $this->numerator*=$sign;
+        }
+        $this->reduction();
+    }
 
-		$this->numerator *= (int)$denominator;
-		$this->denominator *= (int)$numerator;
+    public function realToFraction($number) {
+        $endOfNumber = $number - (int) $number;
+        if ($endOfNumber != 0) {
+            $mul = bcpow(10, strlen($endOfNumber) - 2);
+            return array($number * $mul, $mul);
+        } else {
+            return array($number, 1);
+        }
+    }
 
-		$this->reduction();
-	}
+    public function toString() {
+        $string = '';
+        if ($this->denominator == 1) {
+            $string.=$this->numerator;
+        } else {
+            $string.=$this->numerator . '/' . $this->denominator;
+        }
+        if ($this->mnumerator == 0 && $this->mdenominator == 1) {
+            
+        } else {
+            $string.=($this->mnumerator >= 0 ? '+' : '-');
+            $string.=($this->mdenominator == 1 ? $this->mnumerator . 'M' : $this->mnumerator . '/' . $this->mdenominator . 'M');
+        }
+        return $string;
+    }
 
-	public function toString(){
-		if($this->denominator==1){
-			return $this->numerator;
-		}else{
-			return $this->numerator.'/'.$this->denominator;
-		}
+    public static function highestCommonDivisor($a, $b) {
+        $a = abs($a);
+        while ($a != $b) {
+            if ($a > $b) {
+                $a = $a - $b;
+            } else {
+                $b = $b - $a;
+            }
+        }
+        return $a;
+    }
 
-	}
+    public function isEqual($param) {
+        if ($param instanceof Fraction2) {
+            if ($param->getNumerator() == $this->numerator && $param->getDenominator() == $this->denominator) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif (is_numeric($param)) {
+            if ($this->getRealValue() == $param) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
-	public function minusFraction(){
-		$this->numerator=-$this->numerator;
-	}
+    public function getImproperPart() {
+        if (Fraction2::isPositive(new Fraction2($this->getNumerator(), $this->getDenominator()))) {
+            while ($this->numerator >= $this->denominator) {
+                $this->numerator-=$this->denominator;
+            }
+            $this->minusFraction();
+        } elseif (Fraction2::isNegative(new Fraction2($this->getNumerator(), $this->getDenominator()))) {
+            while ($this->numerator < -$this->denominator) {
+                $this->numerator+=$this->denominator;
+            }
+            $this->add(1);
+            $this->minusFraction();
+        }
+    }
 
-	public function compare($param){
-		if($param instanceof Fraction){
-			$a=$this->numerator*$param->getDenominator();
-			$b=$this->denominator*$param->getNumerator();
-			if($a>$b){
-				return true;
-				//this fraction is bigger
-			}else{
-				return false;
-				//param fraction is bigger
-			}
-		}elseif (is_numeric($param)){
-			$p=$this->realToFraction($param);
-			$a=$this->numerator*$p[1];
-			$b=$this->denominator*$p[0];
-			if($a>$b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-	}
+    public function commonDenominator(&$fraction) {
+        $lcm = $this->leastCommonMultiple($this->denominator, $fraction->denominator);
+        $this->numerator = $this->numerator * ( $lcm / $this->denominator );
+        $fraction->numerator = $fraction->numerator * ( $lcm / $fraction->denominator );
+        $this->denominator = $fraction->denominator = $lcm;
+    }
 
-	public static function isPositive($param){
-		if($param instanceof Fraction){
-			return $param->numerator0 ? true : false;
-		}elseif(is_numeric($param)){
-			return $param>0 ? true : false;
-		}
-	}
+    private function leastCommonMultiple($a, $b) {
+        return ( $a * $b ) / $this->highestCommonDivisor($a, $b);
+    }
 
-	public static function isNegative($param){
-		if($param instanceof Fraction){
-			return $param->numerator<0 ? true : false;
-		}elseif(is_numeric($param)){
-			return $param<0 ? true : false;
-		}
-	}
+    public function getRealValue() {
+        return ($this->mnumerator!=0 ? ($this->mnumerator>0 ? PHP_INT_MAX:~PHP_INT_MAX):$this->numerator/$this->denominator);
+    }
 
-	public function reverse(){
-		$znak=1;
-		$numerator=$this->numerator;
-		$denominator=$this->denominator;
-		if($this->numerator<0){
-			$znak=-1;
-			$this->numerator*=$znak;
-		}
-		$this->numerator=$denominator;
-		$this->denominator=$numerator;
-		if($znak==-1){
-			$this->numerator*=$znak;
-		}
-		$this->reduction();
-	}
+    public static function errormessage($message) {
+        echo '<div class="ui-widget"><div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"><p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span><strong>Alert:</strong>' . $message . '</p></div>';
+    }
+
+    public function minusFraction() {
+        $this->numerator = -$this->numerator;
+        $this->mnumerator = -$this->mnumerator;
+    }
+
+    public function add($param) {
+        if (is_numeric($param)) {
+            $s = new Fraction2($param);
+            $this->add($s);
+        } elseif ($param instanceof Fraction2) {
+            $denominator = $this->denominator * $param->denominator;
+            $numerator = $this->numerator * $param->denominator + $this->denominator * $param->numerator;
+            $this->numerator = $numerator;
+            $this->denominator = $denominator;
+            $mdenominator = $this->mdenominator * $param->mdenominator;
+            $mnumerator = $this->mnumerator * $param->mdenominator + $this->mdenominator * $mparam->numerator;
+            $this->mnumerator = $mnumerator;
+            $this->mdenominator = $mdenominator;
+            $this->reduction();
+        }
+    }
+
+    public function substract($param) {
+        if (is_numeric($param)) {
+            $s = new Fraction2($param);
+            $this->substract($s);
+        } elseif ($param instanceof Fraction2) {
+            $denominator = $this->denominator * $param->denominator;
+            $numerator = $this->numerator * $param->denominator - $this->denominator * $param->numerator;
+            $this->numerator = $numerator;
+            $this->denominator = $denominator;
+            $mdenominator = $this->mdenominator * $param->mdenominator;
+            $mnumerator = $this->mnumerator * $param->mdenominator - $this->mdenominator * $mparam->numerator;
+            $this->mnumerator = $mnumerator;
+            $this->mdenominator = $mdenominator;
+            $this->reduction();
+        }
+    }
+
+    public function multiply($param) {
+        if (is_numeric($param)) {
+            $s = new Fraction2($param);
+            $this->multiply($s);
+        } elseif ($param instanceof Fraction2) {
+            $this->numerator*=$param->numerator;
+            $this->denominator*=$param->denominator;
+            $this->mnumerator*=$param->mnumerator;
+            $this->mdenominator*=$param->mdenominator;
+            $this->reduction();
+        }
+    }
+
+    public function divide($param) {
+        if (is_numeric($param)) {
+            $s = new Fraction2($param);
+            $this->divide($s);
+        } elseif ($param instanceof Fraction2) {
+            $this->numerator*=$param->denominator;
+            $this->denominator*=$param->numerator;
+            $this->mnumerator*=$param->mdenominator;
+            $this->mdenominator*=$param->mnumerator;
+            $this->reduction();
+        } else {
+            $this->errormessage('Must be a fraction or number!');
+        }
+    }
 
 }
 
-$s1=new Fraction(30);
-$s2=new Fraction(5);
-$s1->division($s2);
-echo $s1->toString().'<br/>';
+//$s1 = new Fraction2(0, 1);
+//echo $s1->toString().'<br/>';
+//$s2 = new Fraction2(0, 1, 1, 1);
+//echo $s2->toString().'<br/>';
+//$s3 = new Fraction2(2, 1, 1, 1);
+//echo $s3->toString().'<br/>';
+//$s4 = new Fraction2(2,3);
+//echo $s4->getRealValue() . '<br/>';
 ?>
