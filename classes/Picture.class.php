9e3595607769bcc87bbddda27e14839be8a612c4
Piotr Gołasz <pgolasz@gmail.com>
2013-09-23 14:21:59 +0200

    removing toString() function

--- classes/Picture.class.php
+++ classes/Picture.class.php
@@ -26,7 +26,7 @@ class Picture {
             $handle = ImageCreate(460, 50) or die("Cannot Create image");
             $bg_color = ImageColorAllocate($handle, 255, 255, 255);
             $txt_color = ImageColorAllocate($handle, 0, 0, 0);
-            $text = '(' . $this->wymiar[1]->toString() . ' / ' . $this->wymiar[1]->toString() . ') = 1 ';
+            $text = '(' . $this->wymiar[1] . ' / ' . $this->wymiar[1] . ') = 1 ';
             imagettftext($handle, 18, 0, 0, 35, $txt_color, '../css/Roboto.ttf', $text);
         } elseif ($a == 'r') {
             $handle = ImageCreate(460, 50) or die("Cannot Create image");
@@ -34,13 +34,13 @@ class Picture {
             $txt_color = ImageColorAllocate($handle, 0, 0, 0);
             $c = new Fraction($this->wymiar[0]->getNumerator(), $this->wymiar[0]->getDenominator());
             $c->divide($this->wymiar[1]);
-            $text = '(' . $this->wymiar[0]->toString() . ') / (' . $this->wymiar[1]->toString() . ') = ' . $c->toString();
+            $text = '(' . $this->wymiar[0] . ') / (' . $this->wymiar[1] . ') = ' . $c;
             imagettftext($handle, 18, 0, 0, 35, $txt_color, '../css/Roboto.ttf', $text);
         } elseif ($a == 'c') {
             $handle = ImageCreate(460, 50) or die("Cannot Create image");
             $bg_color = ImageColorAllocate($handle, 255, 255, 255);
             $txt_color = ImageColorAllocate($handle, 0, 0, 0);
-            $text = '(' . $this->wymiar[0]->toString() . ' * 0) = 0';
+            $text = '(' . $this->wymiar[0] . ' * 0) = 0';
             imagettftext($handle, 18, 0, 0, 35, $txt_color, '../css/Roboto.ttf', $text);
         } elseif ($a == 'g') {
             $handle = ImageCreate(490, 50) or die("Cannot Create image");
@@ -49,9 +49,9 @@ class Picture {
             $s = new Fraction($this->wymiar[1]->getNumerator(), $this->wymiar[1]->getDenominator());
             $s->multiply($this->wymiar[2]);
             $s->divide($this->wymiar[3]);
-            $text = $this->wymiar[0]->toString() . ' - (' . $this->wymiar[1]->toString() . ' * ' . $this->wymiar[2]->toString() . ') / (' . $this->wymiar[3]->toString() . ') = ';
+            $text = $this->wymiar[0] . ' - (' . $this->wymiar[1] . ' * ' . $this->wymiar[2] . ') / (' . $this->wymiar[3] . ') = ';
             $this->wymiar[0]->substract($s);
-            $text.=$this->wymiar[0]->toString();
+            $text.=$this->wymiar[0];
             imagettftext($handle, 18, 0, 0, 35, $txt_color, '../css/Roboto.ttf', $text);
         }
         imagecolortransparent($handle, $bg_color);
