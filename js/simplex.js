02fb0698a1a000e0945bf84c1bb94f839477dcce
Piotr Gołasz <pgolasz@gmail.com>
2013-11-26 13:31:29 +0100

    td[data-dane] solves issue with tooltip() throwing exception

--- js/simplex.js
+++ js/simplex.js
@@ -49,7 +49,7 @@ $(document).ajaxStart(function() {
 				success: function(data) {
 					$('#defaultdiv').slideUp('fast');
 					$('#resultdiv2').empty().append(data);
-					$('table.result:gt(0) td').tooltip({
+					$('table.result:gt(0) td[data-dane]').tooltip({
 						delay: 0,
 						showURL: false,
 						fixPNG: true,
@@ -62,8 +62,6 @@ $(document).ajaxStart(function() {
 									'background-color': 'transparent',
 									'text-align': 'center'
 								});
-							}else{
-								return false;
 							}
 						}
 					});
