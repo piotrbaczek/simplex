5f98be783dcbb8ae41da47446bdf37f784b0b296
Piotr Gołasz <pgolasz@gmail.com>
2013-12-13 20:02:59 +0100

    Fixed picking Entry Row on Gomorry method - picks minimal non-negative Fraction

--- sources/receiver.php
+++ sources/receiver.php
@@ -16,14 +16,14 @@ $ss = activity::isactivated2('../activity/active.xml') ? true : false;
 $json = Array();
 if ($ss) {
 	//header for correct json recognition
-	//header('Content-Type: application/json');
+	header('Content-Type: application/json');
 //----------------------------------------------------------------------------
-	$_POST['textarea'] = '2x1+5x2<=30
-2x1+3x2<=26
-0x1+3x2<=15';
-	$_POST['targetfunction'] = '2x1+6x2';
-	$_POST['funct'] = 'true';
-	$_POST['gomorryf'] = 'true';
+//	$_POST['textarea'] = '2x1+5x2<=30
+//2x1+3x2<=26
+//0x1+3x2<=15';
+//	$_POST['targetfunction'] = '2x1+6x2';
+//	$_POST['funct'] = 'true';
+//	$_POST['gomorryf'] = 'false';
 	$tp = new TextareaProcesser(
 			!isset($_POST['textarea']) ? Array() : $_POST['textarea'], !isset($_POST['targetfunction']) ? Array() : $_POST['targetfunction'], !isset($_POST['funct']) ? Array() : $_POST['funct'], !isset($_POST['gomorryf']) ? Array() : $_POST['gomorryf']
 	);
@@ -38,12 +38,10 @@ if ($ss) {
 	try {
 		if ($tp->isCorrect()) {
 			$simplex = new Simplex($tp->getVariables(), $tp->getBoundaries(), $tp->getSigns(), $tp->getTargetfunction(), $tp->getMaxMin(), $tp->getGomorry());
-			//$json[0] = count($tp->getTargetfunction());
-			//$json[1] = $simplex->printProblem() . $simplex->printSolution() . $simplex->printValuePair() . $simplex->printResult();
-			//$json[2] = $simplex->getPrimaryGraphJson();
-			//$json[3] = $simplex->getSecondaryGraphJson();
-			//print_r($simplex->getSecondaryGraphJson());
-			echo $simplex->printSolution();
+			$json[0] = count($tp->getTargetfunction());
+			$json[1] = $simplex->printProblem() . $simplex->printSolution() . $simplex->printValuePair() . $simplex->printResult();
+			$json[2] = $simplex->getPrimaryGraphJson();
+			$json[3] = $simplex->getSecondaryGraphJson();
 		} else {
 			$json[0] = -2;
 			$json[1] = TextareaProcesser::errormessage('Puste dane lub złe dane. Proszę poprawić treść wpisanego zadania.');
@@ -56,6 +54,6 @@ if ($ss) {
 	$json[0] = -1;
 	$json[1] = TextareaProcesser::errormessage('Strona została wyłączona przez administratora.<br/>Prosimy spróbować później.<br/>Powodzenia na egzaminie!');
 }
-//echo json_encode($json);
+echo json_encode($json);
 ?>
 
