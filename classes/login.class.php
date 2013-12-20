3e648a62df4b6807067e793ce8e11417bb65b159
Piotr Gołasz <pgolasz@gmail.com>
2013-12-20 18:40:17 +0100

    checking password function is now static

--- classes/login.class.php
+++ classes/login.class.php
@@ -2,24 +2,20 @@
 
 class login {
 
-    private $password = 'e00fa221684bc62f8de844a062a70a29f13d0e95b6098c26';
-    private $login = 'admin';
+	const password = '85baf8d8d517ab142292d8814be41a60a5e27bc8ba001ac8';
+	const login = 'admin';
 
-    public function __construct($login = '', $password = '') {
-        if (($this->login == $login) && ($this->password == $this->encrypt($password))) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public static function validate($login = '', $password = '') {
+		if ((login::login == $login) && (login::password == login::encrypt($password))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    private function encrypt($password) {
-        return hash('tiger192,4', $password . 'thisisthesalt');
-    }
-
-    public static function getHash($password) {
-        return login::encrypt($password);
-    }
+	public static function encrypt($password) {
+		return hash('tiger192,4', $password . 'thisisthesalt');
+	}
 
 }
 
