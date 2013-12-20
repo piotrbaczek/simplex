f8f19988f2f7c9423334274cc701dfef4f75e0f7
Piotr Gołasz <pgolasz@gmail.com>
2013-12-20 18:40:38 +0100

    checking password function is now static

--- admin/response.php
+++ admin/response.php
@@ -1,11 +1,14 @@
 <?php
 
 include '../classes/login.class.php';
-if (new login($_POST['login'], $_POST['password'])) {
-    session_start();
-    $_SESSION['admin'] = 'true';
-    header('Location:admin.php');
+$login = login::validate($_POST['login'], $_POST['password']);
+if ($login) {
+	session_start();
+	$_SESSION['admin'] = 'true';
+	header('Location:admin.php');
 } else {
-    header('Location:index.php?error=1');
+	$_SESSION['admin'] = 'false';
+	session_destroy();
+	header('Location:index.php?error=1');
 }
 ?>
