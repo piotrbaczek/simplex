dbbe044b8e9702dadca7f8b9230b4c8318ea9459
Piotr Gołasz <pgolasz@gmail.com>
2014-01-17 14:12:11 +0100

    CSVGenerator - code fixes

--- classes/CSVGenerator.class.php
+++ classes/CSVGenerator.class.php
@@ -19,23 +19,23 @@ class CSVGenerator {
 		$this->textarea = $textarea;
 	}
 
-	public function __toString() {
-		$string = '';
+	public function toArray() {
+		$array = Array();
 		if ($this->function == true) {
-			$string.= 'max;';
+			$array[0][0] = 'max';
 		} else {
-			$string.='min;';
+			$array[0][0] = 'min';
 		}
 		if ($this->gomorryFunction == 'true') {
-			$string.='true;';
+			$array[0][1] = 'true';
 		} else {
-			$string.='false;';
+			$array[0][1] = 'false';
 		}
 		$tf = preg_split('/x|\+|\-/', urldecode($this->targetFunction));
+		$ArrayIndex = 2;
 		for ($i = 0; $i < count($tf); $i+=2) {
-			$string.=$tf[$i] . ';';
+			$array[0][$ArrayIndex] = $tf[$i];
 		}
-		$string.="\n";
 		$rows = explode("%0D%0A", $this->textarea);
 		foreach ($rows as $key => $value) {
 			$rows[$key] = urldecode($value);
@@ -44,19 +44,26 @@ class CSVGenerator {
 			$row = preg_split('/=|<=|>=/', $value);
 			$left = preg_split('/x|\+|\-/', $row[0]);
 			for ($i = 0; $i < count($left); $i+=2) {
-				$string.=$left[$i] . ';';
+				$array[$key + 1][] = $left[$i];
 			}
 			if (strpos($value, '<=') !== false) {
-				$string.='<=;';
+				$array[$key + 1][] = '<=';
 			} elseif (strpos($value, '>=') !== false) {
-				$string.='>=;';
+				$array[$key + 1][] = '>=';
 			} else {
-				$string.='=;';
+				$array[$key + 1][] = '=';
 			}
-			$string.=$row[1] . ';';
-			$string.="\n";
+			$array[$key + 1][] = $row[1];
 		}
-		return $string;
+		return $array;
+	}
+
+	public static function outputCSV(Array $data) {
+		$output = fopen("php://output", "w");
+		foreach ($data as $row) {
+			fputcsv($output, $row, ';');
+		}
+		fclose($output);
 	}
 
 }
--- sources/generateCSV.php
+++ sources/generateCSV.php
@@ -2,10 +2,12 @@
 
 header('Content-Type: text/csv; charset=utf-8;');
 header('Content-Disposition: attachment; filename=problem_' . date('Y-m-d') . '.csv');
+header("Pragma: no-cache");
+header("Expires: 0");
 include '../classes/CSVGenerator.class.php';
 if (isset($_GET['funct']) && isset($_GET['gomorryf']) && isset($_GET['targetfunction']) && isset($_GET['textarea'])) {
 	$csvGenerator = new CSVGenerator($_GET['funct'], $_GET['gomorryf'], $_GET['targetfunction'], $_GET['textarea']);
-	echo $csvGenerator;
+	echo CSVGenerator::outputCSV($csvGenerator->toArray());
 } else {
 	echo 'ERROR: Brak poprawnych danych' . print_r($_GET) . ' ;';
 }
