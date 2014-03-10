bf4ff473bdb080896e8e76658c2aaf1f4edc562d
Piotr Gołasz <pgolasz@gmail.com>
2014-03-10 20:34:38 +0100

    Fixed processing fractions in Objective Function

--- classes/Processer.class.php
+++ classes/Processer.class.php
@@ -9,178 +9,177 @@
  */
 class Processer extends Csv_Reader {
 
-	private $table;
-	private $targetfunction = array();
-	private $variables = array();
-	private $signs = array();
-	private $boundaries = array();
-	private $function;
-	private $gomorry;
-	private $znaki = array("<=", ">=", "=");
+    private $table;
+    private $targetfunction = array();
+    private $variables = array();
+    private $signs = array();
+    private $boundaries = array();
+    private $function;
+    private $gomorry;
+    private $znaki = array("<=", ">=", "=");
 
-	/**
-	 * Construct with *.csv file
-	 * @param File $plik
-	 */
-	public function __construct($plik) {
-		parent::__construct($plik);
-		$this->table = parent::get();
-		if ($this->table['0']['0'] != "max" && $this->table['0']['0'] != "min") {
-			$this->errormessage('Nierozpoznane ekstremum funkcji. Pierwsze pole powinno zawierać \'min\' lub \'max\'.');
-		} else {
-			$this->function = $this->table['0']['0'];
-		}
-		if ($this->table['0']['1'] != 'false' && $this->table['0']['1'] != 'true') {
-			$this->errormessage('Nierozpoznane zastosowanie algorytmu Gomorry\'ego. Drugie pole powinno zawierać \'true\' lub \'false\'.');
-		} else {
-			$this->gomorry = ($this->table['0']['1'] == 'true' ? true : false);
-		}
-		$bb = count($this->table['1']);
+    /**
+     * Construct with *.csv file
+     * @param File $plik
+     */
+    public function __construct($plik) {
+        parent::__construct($plik);
+        $this->table = parent::get();
+        if ($this->table['0']['0'] != "max" && $this->table['0']['0'] != "min") {
+            $this->errormessage('Nierozpoznane ekstremum funkcji. Pierwsze pole powinno zawierać \'min\' lub \'max\'.');
+        } else {
+            $this->function = $this->table['0']['0'];
+        }
+        if ($this->table['0']['1'] != 'false' && $this->table['0']['1'] != 'true') {
+            $this->errormessage('Nierozpoznane zastosowanie algorytmu Gomorry\'ego. Drugie pole powinno zawierać \'true\' lub \'false\'.');
+        } else {
+            $this->gomorry = ($this->table['0']['1'] == 'true' ? true : false);
+        }
+        $bb = count($this->table['1']);
 //		for ($i = 1; $i < $bb; $i++) {
 //			if ($bb != count($this->table[$i])) {
 //				$this->errormessage('Równania są różnej długości. Ilość zmiennych równania powinna wynosić ' . $bb . ', a w równaniu ' . $i . ' jest ich ' . count($this->table[$i]));
 //				break;
 //			}
 //		}
-		foreach ($this->table[0] as $key => $value) {
-			if (is_numeric(trim($value))) {
-				if (strpos($value, '/')) {
-					$temp = explode('/', value);
-					$this->targetfunction[] = new Fraction($temp[0], $temp[1]);
-				} else {
-					$this->targetfunction[] = new Fraction(trim($value));
-				}
-			}
-		}
 
-		$aa = count($this->table);
-		$k = 0;
-		for ($i = 1; $i < $aa; $i++) {
-			for ($j = 0; $j < $bb; $j++) {
-				if (in_array($this->table[$i][$j], $this->znaki)) {
-					$this->signs[] = $this->table[$i][$j];
-					$a = trim($this->table[$i][++$j]);
-					if (strpos($a, '/')) {
-						$temp = explode('/', $a);
-						$this->boundaries[] = new Fraction($temp[0], $temp[1]);
-					} else {
-						$this->boundaries[] = new Fraction($a);
-					}
-				} else {
-					$value = trim($this->table[$i][$j]);
-					if (strpos($value, '/')) {
-						$temp = explode('/', $value);
-						$this->variables[$k][$j] = new Fraction($temp[0], $temp[1]);
-					} else {
-						$this->variables[$k][$j] = new Fraction($value);
-					}
-				}
-			}
-			$k++;
-		}
-	}
+        for ($i = 2; $i < count($this->table[0]); $i++) {
+            if (is_numeric(trim($this->table[0][$i]))) {
+                $this->targetfunction[] = new Fraction(trim($this->table[0][$i]));
+            } elseif (strpos($this->table[0][$i], '/')) {
+                $temp = explode('/', $this->table[0][$i]);
+                $this->targetfunction[] = new Fraction($temp[0], $temp[1]);
+            }
+        }
 
-	/**
-	 * Returns array of inputs
-	 * @return Array
-	 */
-	public function getTextareaData() {
-		$array = Array();
-		$array[0] = $this->getMinMax();
-		$array[1] = $this->gomorry ? 'true' : 'false';
-		$string = '';
-		foreach ($this->getTargetFunction() as $key => $value) {
-			$a = clone $value;
-			$a->minusFraction();
-			if ($key == 0) {
-				$string.=$a . 'x' . ($key + 1);
-			} else {
-				if (Fraction::isPositive($a) || Fraction::equalsZero($a)) {
-					$string.='+' . $a . 'x' . ($key + 1);
-				} else {
-					$string.='-' . $a . 'x' . ($key + 1);
-				}
-			}
-		}
-		$array[2] = $string;
-		unset($string);
-		$string = '';
+        $aa = count($this->table);
+        $k = 0;
+        for ($i = 1; $i < $aa; $i++) {
+            for ($j = 0; $j < $bb; $j++) {
+                if (in_array($this->table[$i][$j], $this->znaki)) {
+                    $this->signs[] = $this->table[$i][$j];
+                    $a = trim($this->table[$i][++$j]);
+                    if (strpos($a, '/')) {
+                        $temp = explode('/', $a);
+                        $this->boundaries[] = new Fraction($temp[0], $temp[1]);
+                    } else {
+                        $this->boundaries[] = new Fraction($a);
+                    }
+                } else {
+                    $value = trim($this->table[$i][$j]);
+                    if (strpos($value, '/')) {
+                        $temp = explode('/', $value);
+                        $this->variables[$k][$j] = new Fraction($temp[0], $temp[1]);
+                    } else {
+                        $this->variables[$k][$j] = new Fraction($value);
+                    }
+                }
+            }
+            $k++;
+        }
+    }
 
-		foreach ($this->getVariables() as $key => $value) {
-			foreach ($value as $key2 => $value2) {
-				if ($key2 == 0) {
-					$string.=$value2->getRealValue() . 'x' . ($key2 + 1);
-				} else {
-					if (Fraction::isPositive($value2) || Fraction::equalsZero($value2)) {
-						$string.='+' . $value2->getRealValue() . 'x' . ($key2 + 1);
-					} else {
-						$string.='-' . $value2->getRealValue() . 'x' . ($key2 + 1);
-					}
-				}
-			}
-			$string.=$this->getSigns()[$key] . $this->getBoundaries()[$key] . '&#13;&#10;';
-		}
-		$array[3] = substr($string, 0, -10);
-		unset($string);
-		return $array;
-	}
+    /**
+     * Returns array of inputs
+     * @return Array
+     */
+    public function getTextareaData() {
+        $array = Array();
+        $array[0] = $this->getMinMax();
+        $array[1] = $this->gomorry ? 'true' : 'false';
+        $string = '';
+        foreach ($this->getTargetFunction() as $key => $value) {
+            $a = clone $value;
+            $a->minusFraction();
+            if ($key == 0) {
+                $string.=$a . 'x' . ($key + 1);
+            } else {
+                if (Fraction::isPositive($a) || Fraction::equalsZero($a)) {
+                    $string.='+' . $a . 'x' . ($key + 1);
+                } else {
+                    $string.='-' . $a . 'x' . ($key + 1);
+                }
+            }
+        }
+        $array[2] = $string;
+        unset($string);
+        $string = '';
 
-	/**
-	 * Output Error Message in jQuery UI format
-	 * @param string $message
-	 * @static
-	 */
-	public static function errormessage($message) {
-		echo '<div class="ui-widget"><div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"><p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span><strong>Alert:</strong>' . $message . '</p></div>';
-	}
+        foreach ($this->getVariables() as $key => $value) {
+            foreach ($value as $key2 => $value2) {
+                if ($key2 == 0) {
+                    $string.=$value2->getRealValue() . 'x' . ($key2 + 1);
+                } else {
+                    if (Fraction::isPositive($value2) || Fraction::equalsZero($value2)) {
+                        $string.='+' . $value2->getRealValue() . 'x' . ($key2 + 1);
+                    } else {
+                        $string.='-' . $value2->getRealValue() . 'x' . ($key2 + 1);
+                    }
+                }
+            }
+            $string.=$this->getSigns()[$key] . $this->getBoundaries()[$key] . '&#13;&#10;';
+        }
+        $array[3] = substr($string, 0, -10);
+        unset($string);
+        return $array;
+    }
 
-	/**
-	 * Return array of elements from target function
-	 * @return array
-	 */
-	public function getTargetFunction() {
-		return $this->targetfunction;
-	}
+    /**
+     * Output Error Message in jQuery UI format
+     * @param string $message
+     * @static
+     */
+    public static function errormessage($message) {
+        echo '<div class="ui-widget"><div class="ui-state-error ui-corner-all" style="padding: 0 .7em;"><p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span><strong>Alert:</strong>' . $message . '</p></div>';
+    }
 
-	/**
-	 * Returns array of LP coefficients
-	 * @return array
-	 */
-	public function getVariables() {
-		return $this->variables;
-	}
+    /**
+     * Return array of elements from target function
+     * @return array
+     */
+    public function getTargetFunction() {
+        return $this->targetfunction;
+    }
 
-	/**
-	 * Returns array of enumSign objects - signs from [<=,>=,=]
-	 * @return array
-	 */
-	public function getSigns() {
-		return $this->signs;
-	}
+    /**
+     * Returns array of LP coefficients
+     * @return array
+     */
+    public function getVariables() {
+        return $this->variables;
+    }
 
-	/**
-	 * Returns boundaries of each LP equation
-	 * @return array
-	 */
-	public function getBoundaries() {
-		return $this->boundaries;
-	}
+    /**
+     * Returns array of enumSign objects - signs from [<=,>=,=]
+     * @return array
+     */
+    public function getSigns() {
+        return $this->signs;
+    }
 
-	/**
-	 * Returns true/false if Gomory's Cutting Plane Algorithm should be used
-	 * @return boolean
-	 */
-	public function getGomorry() {
-		return $this->gomorry;
-	}
+    /**
+     * Returns boundaries of each LP equation
+     * @return array
+     */
+    public function getBoundaries() {
+        return $this->boundaries;
+    }
 
-	/**
-	 * Returns true if max, false if min
-	 * @return boolean
-	 */
-	public function getMinMax() {
-		return $this->function;
-	}
+    /**
+     * Returns true/false if Gomory's Cutting Plane Algorithm should be used
+     * @return boolean
+     */
+    public function getGomorry() {
+        return $this->gomorry;
+    }
+
+    /**
+     * Returns true if max, false if min
+     * @return boolean
+     */
+    public function getMinMax() {
+        return $this->function;
+    }
 
 }
 ?>
